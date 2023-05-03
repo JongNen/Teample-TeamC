c46420169b117d1c3fb62fc0c5733c4b1fd214ee
abc <abc@gmail.com>
2023-05-04 08:37:59 +0900

    충돌 부분수정

--- src/main/java/controller/write/writeController.java
+++ src/main/java/controller/write/writeController.java
@@ -9,27 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import data.User;
-
-
+//글쓰기 컨트롤러
+@WebServlet("/write")
 public class writeController extends HttpServlet {
 
 		@Override
 		protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-//			//글쓰기 컨트롤러
-//			req.setCharacterEncoding("utf-8");
-//			
-//			User logonUser = (User)req.getSession().getAttribute("logonUser");
-//			
-//			String target=req.getParameter("target");
-//			String writer=logonUser.getName();
-//			String title=req.getParameter("title");
-//			String postBody=req.getParameter("postBody");
-//			String IMG=req.getParameter("IMG");
-//			
-//			System.out.println("writer="+writer);
-//			
-//			int r=WriteDAO.createReview(target, writer, title, postBody, IMG);
 		
 			req.getRequestDispatcher("/WEB-INF/views/write.jsp").forward(req, resp);
 		}
